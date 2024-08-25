71aba2eddc201e889493599c68a97163bb9c0927
Nahombadebo47 <nahombadebo47@gmail.com>
2024-08-25 13:38:38 +0300

    Dojo-blog app almost finalized with different modules and functionality added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 function App() {
   return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/blogs/:id' element={<BlogDetails />} />
+          </Routes>
+        </div>
 
-    <div className="App">
-      <div className="className">
-        <h1>App Componenet</h1>
       </div>
-
-    </div>
+    </Router >
 
   );
 }
---
+++ src/Create.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario')
+    const handleSubmit = (e) => {
+    }
+
+    return (
+        <div className="create">
+            <form onSubmit={handleSubmit}>
+                <label>Blog title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog body</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author: </label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+        </div>
+    );
+}
+
+export default Create;
